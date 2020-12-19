f47dc53f9e3df23dd4a593c1d501fa1babcf821c
mou-min <74852472+mou-min@users.noreply.github.com>
2020-12-19 09:27:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/fw876/helloworld package/helloworld
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
